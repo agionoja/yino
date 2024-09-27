f8d3757c2e4036a70dc483c69a99cc5dd25c2afb
agionoja <pauldivineagionoja@gmail.com>
2024-09-27 11:08:29 +0100

    Chore: added a background image and gradient to the auth route

--- app/routes/_auth/route.tsx
+++ app/routes/_auth/route.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "@remix-run/react";
+import authBg from "~/assets/images/auth-bg.png";
 
 export default function AuthLayout() {
   return (
     <main
+      style={{
+        backgroundImage: `linear-gradient(rgba(205, 236, 248, 0.95), rgba(244, 245, 247, 0.98)), url(${authBg})`,
+      }}
       className={
-        "flex min-h-screen w-full items-center justify-center py-10 md:py-20"
+        "flex min-h-screen w-full items-center justify-center bg-cover bg-center bg-no-repeat py-10 md:py-20"
       }
     >
       <div
